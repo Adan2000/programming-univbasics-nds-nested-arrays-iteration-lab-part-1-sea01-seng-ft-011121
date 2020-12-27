996c8438a4370c0745055204c407419ab8d8bb43
Adan Escamilla <adanescamilla580@gmail.com>
2020-12-27 20:50:04 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,9 +1,21 @@
-require 'pry'
-def find_even_values(src)
-  binding.pry
-row_index = 0
-while row_index < src.count do 
 
+def find_even_values(src)
+row_index = 0
+
+while row_index < src.count do
+  
+element_index = 0
+
+while element_index < src[row_index].count do 
+  
+  if [row_index][element_index].even?
+    p src[row_index][element_index]
+
+end
+
+element_index += 1
+
+end
 
 end
 
